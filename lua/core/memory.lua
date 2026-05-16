7759a49184518abc0af9a31169881abf3ad3ce47
Veddy1674 <veddytasses@gmail.com>
2026-05-16 16:06:28 +0200

    renamed read/write functions

--- lua/core/memory.lua
+++ lua/core/memory.lua
@@ -5,37 +5,39 @@ local memory = {}
 ---@diagnostic disable-next-line: undefined-global
 local native = assert(native, "native module not found")
 
+-- NOTE: all functions are little-endian based
+
 ---@param addr number Address
-function memory.read_s8_le(addr)
-    return native.read_s8_le(addr)
+function memory.read_s8(addr)
+    return native.read_s8(addr)
 end
 
 ---@param addr number Address
-function memory.read_s16_le(addr)
-    return native.read_s16_le(addr)
+function memory.read_s16(addr)
+    return native.read_s16(addr)
 end
 
 ---@param addr number Address
-function memory.read_s32_le(addr)
-    return native.read_s32_le(addr)
-end
-
----@param addr number Address
----@param value number Value to write
-function memory.write_s8_le(addr, value)
-    native.write_s8_le(addr, value)
+function memory.read_s32(addr)
+    return native.read_s32(addr)
 end
 
 ---@param addr number Address
 ---@param value number Value to write
-function memory.write_s16_le(addr, value)
-    native.write_s16_le(addr, value)
+function memory.write_s8(addr, value)
+    native.write_s8(addr, value)
 end
 
 ---@param addr number Address
 ---@param value number Value to write
-function memory.write_s32_le(addr, value)
-    native.write_s32_le(addr, value)
+function memory.write_s16(addr, value)
+    native.write_s16(addr, value)
+end
+
+---@param addr number Address
+---@param value number Value to write
+function memory.write_s32(addr, value)
+    native.write_s32(addr, value)
 end
 
 return memory
